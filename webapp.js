b31219c02075ca8715dd18102ba75fa4b51dcca9
Eric Fruin <eric.fruin@warriorlife.net>
2018-01-23 14:38:12 -0800

    Animated Button
    * Added animated button
    * Changed .gif frames into .pngs's

--- webapp.js
+++ webapp.js
@@ -1,6 +1,13 @@
 // JavaScript Document
+
+// Score
 var score = 0;
+var x = 0;
+
 function memeclicker() {
-	score= score + 1
+	if( x == 5 ) { x = 0; }
+		else { x = x + 1; }
+	score = score + 1;
 	document.getElementById("score").value=score;
-}
+	document.getElementById("ugandan").firstChild.setAttribute("src", "frames/" + x + ".png");
+} 
