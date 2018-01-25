5b83358dcde46dbaa4007a12f5eabe47f157c0ba
Eric Fruin <eric.fruin@warriorlife.net>
2018-01-25 14:45:44 -0800

    Generators
    * added a lot

--- webapp.js
+++ webapp.js
@@ -2,12 +2,50 @@
 
 // Score
 var score = 0;
-var x = 0;
+var frame = 0;
+
+// Generators
+var nut = 0;
+var spaghett = 0;
+var mistabean = 0;
 
 function memeclicker() {
-	if( x == 5 ) { x = 0; }
-		else { x = x + 1; }
+	if( frame == 5 ) { frame = 0; }
+		else { frame = frame + 1; }
 	score = score + 1;
 	document.getElementById("score").value=score;
-	document.getElementById("ugandan").firstChild.setAttribute("src", "frames/" + x + ".png");
-} 
+	document.getElementById("ugandan").firstChild.setAttribute("src", "frames/" + frame + ".png");
+}
+
+function buy(sel) {
+	switch(sel) {
+		case "nut":
+			if( score >= 10 ) {
+				score -=10;
+				nut++;
+			}
+			else {
+				alert("not enough money!");
+			}
+			break;
+		case "spaghett":
+			if( score >= 100 ) {
+				score -=100;
+				spaghett++;
+			}
+			else {
+				alert("not enough money!");
+			}
+			break;
+		case "mistabean":
+			if( score >= 1000 ) {
+				score -=1000;
+				mistabean++;
+			}
+			else {
+				alert("not enough money!");
+			}
+			break;
+	}
+	document.getElementById("score").value=score;
+}
