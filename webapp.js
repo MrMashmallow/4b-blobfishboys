0eba7b27a362641c76e74765f43d294a887740eb
Roy Park <roy.park@warriorlife.net>
2018-03-01 11:30:08 -0800

    Commented on JS and css changes
    I changed the font and center of the css and commented on the Javascript where it wasnt.

--- webapp.js
+++ webapp.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
+
 // Score
 var score = 0;
 var frame = 0;
@@ -12,6 +13,7 @@ var robloks = 0;
 var flex_tape = 0;
 var de_wae = 0;
 
+//Ables the Ugandan warrior to move in frames after each click
 function memeclicker() {
 	if( frame == 5 ) { frame = 0; }
 		else { frame = frame + 1; }
@@ -22,56 +24,73 @@ function memeclicker() {
 
 function buy(sel) {
 	switch(sel) {
+		//tells the value of the nut and its function
 		case "nut":
 			if( score >= 10 ) {
 				score -=10;
 				nut++;
 			}
+			// alerts the player that they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+		
+		//Tells the value of the spaghett and its function
 		case "spaghett":
 			if( score >= 100 ) {
 				score -=100;
 				spaghett++;
 			}
+		   // alerts the player that they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//Tells the value of mistabean and its function
 		case "mistabean":
 			if( score >= 1000 ) {
 				score -=1000;
 				mistabean++;
 			}
+		// alerts the player that they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//Tells the value of robloks and its function
 		case "robloks":
 			if( score >= 10000 ) {
 				score -=10000;
 				robloks++;
 			}
+       // alerts the player that they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//Tells the value of flex_tape and its function
 		case "flex_tape":
 			if( score >= 100000 ) {
 				score -=100000;
 				flex_tape++;
 			}
+	 // alerts the player that they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//Tells the value of de_wae and its function
 		case "de_wae":
 			if( score >= 1000000 ) {
 				score -=1000000;
 				de_wae++;
 			}
+			
 			else {
 				alert("not enough money!");
 			}
