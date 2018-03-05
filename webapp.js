774d278f2e7684c29efb9002620c338f37e5f9bd
Roy Park <roy.park@warriorlife.net>
2018-03-05 11:00:14 -0800

    Commented and changed files
    I had all the text and the buttons to be aligned in the center. I also added the values on the screen for the buttons as I noticed you did not have any. Made the text black as the text was hard to see in the sky background. I commented on your javascript and somewhat made it organized. I gave you an introduction to create a reset button later if you want to implement cookies.

--- webapp.js
+++ webapp.js
@@ -12,6 +12,7 @@ var robloks = 0;
 var flex_tape = 0;
 var de_wae = 0;
 
+//frame movment for the ugandan warrior
 function memeclicker() {
 	if( frame == 5 ) { frame = 0; }
 		else { frame = frame + 1; }
@@ -22,56 +23,73 @@ function memeclicker() {
 
 function buy(sel) {
 	switch(sel) {
+		//tells the value for the nut and its function
 		case "nut":
 			if( score >= 10 ) {
 				score -=10;
 				nut++;
 			}
+			//alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//tells the value for the spaghett and its function
 		case "spaghett":
 			if( score >= 100 ) {
 				score -=100;
 				spaghett++;
 			}
+			//alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//tells the value for mistabean and its function
 		case "mistabean":
 			if( score >= 1000 ) {
 				score -=1000;
 				mistabean++;
 			}
+			 //alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//tells the value for robloks and its function
 		case "robloks":
 			if( score >= 10000 ) {
 				score -=10000;
 				robloks++;
 			}
+			//alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//tells the value for the flex_tape and its function
 		case "flex_tape":
 			if( score >= 100000 ) {
 				score -=100000;
 				flex_tape++;
 			}
+			//alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
 			break;
+			
+			//tells the value for de_wae and its function
 		case "de_wae":
 			if( score >= 1000000 ) {
 				score -=1000000;
 				de_wae++;
 			}
+			//alerts the player they do not have enough currency
 			else {
 				alert("not enough money!");
 			}
@@ -94,4 +112,9 @@ function bonus() {
 
 function update_values() {
 	document.getElementById("score").value=Math.floor(score);
+}
+
+function resetGame() {
+	// if cookies are implemented this needs to be modified!!
+	
 }
